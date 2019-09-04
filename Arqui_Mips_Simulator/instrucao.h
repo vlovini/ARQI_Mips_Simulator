b5f6ad810485a4f9c87994b3ab634493c36539eb
Martin Alejandro Bernasconi Martinez <MARTINBM@edu.unisinos.br>
2019-09-03 21:40:52 -0300

    instrucao

--- Arqui_Mips_Simulator/instrucao.h
+++ Arqui_Mips_Simulator/instrucao.h
@@ -1,11 +1,20 @@
 #ifndef INSTRUCAO_H
 #define INSTRUCAO_H
-
+#include <QString>
 
 class Instrucao
 {
+private:
+    QString operacao;
+    QString op1;
+    QString op2;
+    QString op3;
 public:
-    Instrucao();
+    Instrucao(QString inst);
+    QString getOperacao() const;
+    QString getOp1() const;
+    QString getOp2() const;
+    QString getOp3() const;
 };
 
 #endif // INSTRUCAO_H
--- Arqui_Mips_Simulator/mainwindow.cpp
+++ Arqui_Mips_Simulator/mainwindow.cpp
@@ -1,11 +1,17 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "instrucao.h"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    Instrucao minhaIntrucao("addi $t1, $t2, 1");
+    QString operacao = minhaIntrucao.getOperacao();
+    operacao = minhaIntrucao.getOp1();
+    operacao = minhaIntrucao.getOp2();
+    operacao = minhaIntrucao.getOp3();
 }
 
 MainWindow::~MainWindow()
